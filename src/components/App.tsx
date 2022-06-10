2cae4ddbfbf0af2568157bf9ac01c8c80041a956
ChrisCoastal <contactchriscoastal@gmail.com>
2022-06-09 22:43:33 -0700

    add react-dnd

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+
+import { DndProvider } from 'react-dnd'
+import { TouchBackend } from 'react-dnd-touch-backend'
 import GameBoard from './GameBoard/GameBoard'
 
 function App() {
   return (
     <div className="App">
-      <GameBoard />
+      <DndProvider backend={TouchBackend}>
+        <GameBoard />
+      </DndProvider>
     </div>
   )
 }
